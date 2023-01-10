12ecfc9c5a25ecb08d0f639b7ecfcc75e2290ebd
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-01-10 14:27:33 +0100

    Add processOrder function

--- src/js/script.js
+++ src/js/script.js
@@ -75,7 +75,7 @@
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
       /* find menu container */
       const menuContainer = document.querySelector(select.containerOf.menu);
-      console.log('menuContainer', menuContainer);
+      // console.log('menuContainer', menuContainer);
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
@@ -84,9 +84,9 @@
     
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
-      console.log('thisproduct form',thisProduct.form);
+      // console.log('thisproduct form',thisProduct.form);
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
-      console.log('inputy i selecty', thisProduct.formInputs);
+      // console.log('inputy i selecty', thisProduct.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
     }
@@ -95,7 +95,7 @@
     
       // /* find the clickable trigger */
       // const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      // // console.log('clickableTrigger', clickableTrigger);
+      // console.log('clickableTrigger', clickableTrigger);
       /*Start: add event listener to clickable trigger on event click */
       thisProduct.accordionTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
@@ -116,7 +116,7 @@
     }
     initOrderForm (){
       const thisProduct = this;
-      console.log('thisproduct orderform', thisProduct);
+      // console.log('thisproduct orderform', thisProduct);
       thisProduct.form.addEventListener('submit', function(event){
         event.preventDefault();
         thisProduct.processOrder();
@@ -133,9 +133,48 @@
     }
     processOrder (){
       const thisProduct = this;
-      console.log('thisproduct procesOrder',thisProduct);
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+      // set price to default price
+      let price = thisProduct.data.price;
+      
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log('paramId',paramId, 'param', param);
 
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          
+          // console.log('option', option);
+          // console.log('optionId',optionId);
+          // console.log('option',option);
+          // console.log('formData[paramId]', formData[paramId]);
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if (formData[paramId] && formData[paramId].includes(optionId)){
+            
+            // check if the option is not default
+            if(!option.default){
+              console.log('sraka');
+              price += option.price;
+              console.log('optiondefault', option.default);
+              
+            }
+          }
+          else if(option.default){
+            price -= option.price;
+          }
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
+    
    
   }
   const app = {
