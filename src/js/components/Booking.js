5bba96b797600e9082b8dcd85a35f11370c23cf3
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-02-03 12:31:10 +0100

    Add initWidgets method

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,6 @@
-import { templates } from '../settings.js';
+import { select, templates } from '../settings.js';
 import utils from '../utils.js';
+import AmountWidget from './AmountWidget.js';
 
 
 class Booking{
@@ -14,9 +15,32 @@ class Booking{
     const generatedHtml = templates.bookingWidget();
     thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
+    
     const bookingElement = utils.createDOMFromHTML(generatedHtml);
     thisBooking.dom.wrapper.appendChild(bookingElement);
 
+    thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+
+    console.log(thisBooking.dom.peopleAmount);
+    console.log(thisBooking.dom.hoursAmount);
+
+  }
+  initWidgets(){
+    const thisBooking = this;
+    
+    new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.dom.peopleAmount.addEventListener('click', function(event){
+      event.preventDefault();
+      console.log('cokolwiek');
+    });
+
+
+    new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.hoursAmount.addEventListener('click', function(event){
+      event.preventDefault();
+      console.log('bleeee');
+    });
   }
 }
 
